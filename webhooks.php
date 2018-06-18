020ccd9131da6927352ea1de980c692961d1ab7f
kenkm <40350751+kenkm@users.noreply.github.com>
2018-06-18 14:38:27 +0800

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'BBObhSYVl0ErLvWtQ/kgUd4o0izSoQxNdR57tIH3Bf5+Z2vaacs9XrURcvi55LU/VmiUOeQjYV8U0Nstd58N/ZFq7l2V1eS24qWEIXpSH/DK4KjhbHDn4dcVxigYkFp6FMH/R9slw7r0cVdY+39QdQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
